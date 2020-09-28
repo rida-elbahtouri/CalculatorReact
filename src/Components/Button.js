9d4c8d0a56513e0ba845676b9f02d2d2643598d5
rida <elbahtouri0123@gmail.com>
2020-09-28 14:31:13 +0100

    pass colors and wide as props to buttons

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,16 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
-const Display = (props) => {
-  const { name } = props;
+const Button = (props) => {
+  const { name, color, wide } = props;
   return (
-    <button type="button" name={name} id={name} className="button">
+    <button
+      type="button"
+      style={{ backgroundColor: color, width: wide }}
+      name={name}
+      id={name}
+      className="button"
+    >
       {name}
     </button>
   );
 };
 
-Display.propTypes = {
+Button.propTypes = {
   name: PropTypes.string,
+  color: PropTypes.string,
+  wide: PropTypes.string,
 };
 
-export default Display;
+Button.defaultProps = {
+  color: "#F08D45",
+  wide: "25%",
+};
+export default Button;
--- src/Components/ButtonPanel.js
+++ src/Components/ButtonPanel.js
@@ -1,39 +1,39 @@
 import React from "react";
 import Button from "./Button";
-
 const ButtonPanel = () => {
   return (
     <div className="calucator-buttons">
       <div className="row">
-        <Button name="AC" />
-        <Button name="+/-" />
-        <Button name="%" />
+        <Button color="#DCDCDC" name="AC" />
+        <Button color="#DCDCDC" name="+/-" />
+        <Button color="#DCDCDC" name="%" />
         <Button name="÷" />
       </div>
       <div className="row">
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
+        <Button color="#DCDCDC" name="7" />
+        <Button color="#DCDCDC" name="8" />
+        <Button color="#DCDCDC" name="9" />
         <Button name="x" />
       </div>
       <div className="row">
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
+        <Button color="#DCDCDC" name="4" />
+        <Button color="#DCDCDC" name="5" />
+        <Button color="#DCDCDC" name="6" />
         <Button name="-" />
       </div>
       <div className="row">
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
+        <Button color="#DCDCDC" name="1" />
+        <Button color="#DCDCDC" name="2" />
+        <Button color="#DCDCDC" name="3" />
         <Button name="+" />
       </div>
       <div className="row">
-        <Button name="0" />
-        <Button name="." />
+        <Button color="#DCDCDC" wide="50%" name="0" />
+        <Button color="#DCDCDC" name="." />
         <Button name="=" />
       </div>
     </div>
   );
 };
+
 export default ButtonPanel;
