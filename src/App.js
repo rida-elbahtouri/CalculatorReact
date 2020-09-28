8c143ad78f6a3d1e218539ce85ad1aaee9b3c4eb
rida <elbahtouri0123@gmail.com>
2020-09-28 10:21:12 +0100

    fix linter issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
-export default () => {
+const App = () => {
   return (
     <div>
       <h1>Hello React</h1>
     </div>
   );
 };
+export default App;
